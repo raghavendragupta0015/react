f3c86eca50cb830d47f3fe17b71b4fe64b01a5d9
Raghavendra Gupta <raghavendragupta0015@gmail.com>
2026-03-27 19:54:06 +0530

    localstorage-complete

---
+++ 12-local-storage/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const App = () => {
+  // localStorage.setItem();
+  // localStorage.getItem();
+  // localStorage.removeItem();
+  // localStorage.clear();
+
+  /* to store objetcs or arrays in localStorage */
+  // const user = {
+  //   username: "sai",
+  //   age: 19,
+  //   profession: "student",
+  // };
+
+  // console.log(user);
+  // localStorage.setItem("user", JSON.stringify(user));
+
+  /* to retrive objetcs or arrays in localStorage */
+  // const user = JSON.parse(localStorage.getItem("user"));
+  // console.log(user);
+
+  return <div>App</div>;
+};
+
+export default App;
